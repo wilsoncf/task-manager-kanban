fadcf916096ee172f00d1b6a70fdd50683181ed7
Wilson <wilsoncffilho@outlook.com>
2026-02-01 23:17:58 -0300

    feat: setup spring boot backend

---
+++ backend/src/test/java/com/wilsoncf/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsoncf.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
